8d1ff2709a1237dde0950c04d8445c4adf7ff8ef
Upendra Yanala <Upendra.Yanala2@mvwc.com>
2021-05-11 00:05:25 -0400

    commented source code hash

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "cloudfront_lambda" {
   function_name    = "cloudfront_lambda"
   role             = aws_iam_role.iam_for_lambda_tf.arn
   handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  #source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "nodejs12.x"
   publish          = true
 }
