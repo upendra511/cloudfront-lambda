3a80f5245fee46f5efd61c4cee4e08bbe9d19bda
Upendra Yanala <Upendra.Yanala2@mvwc.com>
2021-05-12 15:58:59 -0400

    added new content

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 'Hello World' nodejs6.10 runtime AWS Lambda function
 exports.handler = (event, context, callback) => {
-    console.log('Hello,Hello,Hello, logs!');
+    console.log('Hello,Hello,Upendra, logs!');
     callback(null, 'great success');
 }
