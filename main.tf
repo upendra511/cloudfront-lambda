ab92cfa480960207b2e273ac436829fec9473b16
Upendra Yanala <Upendra.Yanala2@mvwc.com>
2021-05-12 15:53:23 -0400

    added second cloudfront distro

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_lambda_function" "cloudfront_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "cloudfront_lambda"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
-  handler          = "index.handler"
+  filename      = "lambda_function.zip"
+  function_name = "cloudfront_lambda"
+  role          = aws_iam_role.iam_for_lambda_tf.arn
+  handler       = "index.handler"
   #source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs12.x"
-  publish          = true
+  runtime = "nodejs12.x"
+  publish = true
 }
 
 resource "aws_iam_role" "iam_for_lambda_tf" {
@@ -130,6 +130,82 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution2" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/E3GKCMPFJ1RH65"
+    }
+  }
+  enabled = true
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = aws_lambda_function.cloudfront_lambda.qualified_arn
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = aws_lambda_function.cloudfront_lambda.qualified_arn
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+}
+
+
 resource "null_resource" "cache-invalidate" {
   provisioner "local-exec" {
     command = "aws cloudfront create-invalidation --distribution-id E146LF2IKNT4C1 --paths /*"
